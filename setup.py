5859b7747da1c29ac2073c50ba5ede51ca0c9c51
stephenwithph <stephenwithph@users.noreply.github.com>
2018-05-09 21:19:23 -0700

    make setup.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='samplelibrary',
+    version='0.0.1',
+    description='a silly thing to exercise what might be a bug',
+    packages=find_packages(exclude=['*test']),
+    install_requires=[
+        'protobuf>=3.5.2',
+    ],
+)
