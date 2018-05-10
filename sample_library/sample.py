b8d8ae794230e715fb61f255d0e7afeaa737258b
stephenwithph <stephenwithph@users.noreply.github.com>
2018-05-09 21:10:56 -0700

    write silly implementation

---
+++ sample_library/sample.py
@@ -0,0 +1,13 @@
+from sample_library.foo_pb2 import Foo
+
+def __write():
+    foo = Foo(foo=42, bar=3.14, baz=True)
+
+    with open('foo.pb', 'wb') as f:
+        f.write(foo.SerializeToString())
+
+def read():
+    with open('foo.pb', 'rb') as f:
+        foo = Foo().FromString(f.read())
+        print(foo)
+        return foo
