b50d9d6f9345de3ef04734dccdbcdd470631d4aa
stephenwithph <stephenwithph@users.noreply.github.com>
2018-05-09 21:30:33 -0700

    fix excluding tests

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ setup(
     name='samplelibrary',
     version='0.0.1',
     description='a silly thing to exercise what might be a bug',
-    packages=find_packages(exclude=['*test']),
+    packages=find_packages(exclude=['*tests*']),
     install_requires=[
         'protobuf>=3.5.2',
     ],
